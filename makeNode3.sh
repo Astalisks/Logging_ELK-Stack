e3244db61c3fbfd91ed316954de29f33ac87ab22
Astalisks <astalisksit@gmail.com>
2024-01-17 18:09:54 +0900

    [ ]がsedコマンドの正規表現に引っかかった

--- makeNode3.sh
+++ makeNode3.sh
@@ -6,5 +6,5 @@ sudo sed -i 's/#node.name: node-1/node.name: node-3/' /etc/elasticsearch/elastic
 sudo sed -i 's|path.data: /var/lib/elasticsearch|path.data: /var/lib/elasticsearch01|' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's|path.logs: /var/log/elasticsearch|path.logs: /var/log/elasticsearch01|' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/#discovery.seed_hosts: ["host1", "host2"]/discovery.seed_hosts: ["node-1", "node-2", "node-3"]/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/#cluster.initial_master_nodes: ["node-1", "node-2"]/cluster.initial_master_nodes: ["node-1", "node-2", "node-3"]/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.seed_hosts: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#cluster.initial_master_nodes: \["node-1", "node-2"\]/cluster.initial_master_nodes: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
