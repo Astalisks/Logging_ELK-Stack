410160275349547929278609e7eddf4b4f42c2fb
Astalisks <astalisksit@gmail.com>
2024-01-17 19:24:34 +0900

    こうなったら全部削除だ

--- remove.sh
+++ remove.sh
@@ -3,22 +3,32 @@
 # Elasticsearchのアンインストール
 sudo systemctl stop elasticsearch.service
 sudo apt-get remove --purge -y elasticsearch
+sudo rm -rf /var/lib/elasticsearch
+sudo rm -rf /var/log/elasticsearch
 
 # Kibanaのアンインストール
 sudo systemctl stop kibana.service
 sudo apt-get remove --purge -y kibana
+sudo rm -rf /var/lib/kibana
+sudo rm -rf /var/log/kibana
 
 # Logstashのアンインストール
 sudo systemctl stop logstash.service
 sudo apt-get remove --purge -y logstash
+sudo rm -rf /var/lib/logstash
+sudo rm -rf /var/log/logstash
 
 # Filebeatのアンインストール
 sudo systemctl stop filebeat
 sudo apt-get remove --purge -y filebeat
+sudo rm -rf /var/lib/filebeat
+sudo rm -rf /var/log/filebeat
 
 # Metricbeatのアンインストール
 sudo systemctl stop metricbeat
 sudo apt-get remove --purge -y metricbeat
+sudo rm -rf /var/lib/metricbeat
+sudo rm -rf /var/log/metricbeat
 
 # すべての不要なパッケージと依存関係の削除
 sudo apt autoremove -y
