9aa504faa2f786204048a7a6f4ddf8074c9f4f2a
Astalisks <astalisksit@gmail.com>
2024-01-17 19:06:38 +0900

    01いらんかった

--- makeNode3.sh
+++ makeNode3.sh
@@ -3,8 +3,6 @@
 # Elasticsearchの設定ファイルを編集
 sudo sed -i 's/#cluster.name: my-application/cluster.name: my-cluster01/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#node.name: node-1/node.name: node-3/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's|path.data: /var/lib/elasticsearch|path.data: /var/lib/elasticsearch01|' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's|path.logs: /var/log/elasticsearch|path.logs: /var/log/elasticsearch01|' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.seed_hosts: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#cluster.initial_master_nodes: \["node-1", "node-2"\]/cluster.initial_master_nodes: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
