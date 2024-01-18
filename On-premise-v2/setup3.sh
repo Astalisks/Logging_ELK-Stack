b1822e893b93a3afa276307a1c49ba2393fee5c6
Astalisks <astalisksit@gmail.com>
2024-01-18 16:59:39 +0900

    4台目動くかな

--- On-premise-v2/setup3.sh
+++ On-premise-v2/setup3.sh
@@ -30,7 +30,7 @@ sudo apt-get update
 sudo apt-get install -y elasticsearch
 # Elasticsearchの設定ファイルを編集
 sudo sed -i 's/#cluster.name: my-application/cluster.name: my-cluster01/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/#node.name: node-3/node.name: node-1/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#node.name: node-1/node.name: node-3/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.seed_hosts: \["10.252.0.206", "10.252.0.208", "10.252.0.234"\]/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#cluster.initial_master_nodes: \["node-1", "node-2"\]/cluster.initial_master_nodes: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
