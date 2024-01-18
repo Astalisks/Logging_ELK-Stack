d1389443ad4ec4e262a739f61c9ecd15550a09cf
Astalisks <astalisksit@gmail.com>
2024-01-18 14:10:57 +0900

    elasticsearchはこれでいいかな

--- On-premise-v2/setup1.sh
+++ On-premise-v2/setup1.sh
@@ -32,7 +32,7 @@ sudo apt-get install -y elasticsearch
 sudo sed -i 's/#cluster.name: my-application/cluster.name: my-cluster01/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#node.name: node-1/node.name: node-1/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.seed_hosts: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.seed_hosts: \["10.252.0.206", "10.252.0.208", "10.252.0.234"\]/' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#cluster.initial_master_nodes: \["node-1", "node-2"\]/cluster.initial_master_nodes: \["node-1", "node-2", "node-3"\]/' /etc/elasticsearch/elasticsearch.yml
 echo "action.auto_create_index: *" >> /etc/elasticsearch/elasticsearch.yml
 sudo update-rc.d elasticsearch defaults 95 10
@@ -43,6 +43,9 @@ sudo /bin/systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
 # sudo -i service elasticsearch start
 
+# ログ確認
+# sudo journalctl -f
+# sudo journalctl --unit elasticsearch
 
 
 # Kibana setup
