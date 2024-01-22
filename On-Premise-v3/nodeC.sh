508f576c01dfd304755b9f6bafb95be19119bef0
Astalisks <astalisksit@gmail.com>
2024-01-22 19:44:13 +0900

    .shたりんかった

--- On-Premise-v3/nodeC.sh
+++ On-Premise-v3/nodeC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # /etc/hosts に追加するエントリー
-.entry_etc_hosts # 関数を外部ファイルから読み込む
-add_if_not_exists "10.252.0.206" "node-A"
-add_if_not_exists "10.252.0.208" "node-B"
-add_if_not_exists "10.252.0.239" "node-C"
+./entry_etc_hosts.sh
+entry_etc_hosts "10.252.0.206" "node-A"
+entry_etc_hosts "10.252.0.208" "node-B"
+entry_etc_hosts "10.252.0.239" "node-C"
 
 # Elasticsearchの設定ファイルの編集
 sudo sed -i 's/#cluster.name: my-application/cluster.name: my-cluster02/' /etc/elasticsearch/elasticsearch.yml
